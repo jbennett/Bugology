a287ea0b17a4fab309bb41b85485cbce71912526
Jonathan Bennett <jonathan@jbennett.me>
2016-01-25 22:28:47 -0500

    only show open issues

--- Bugology/IssuesViewController.swift
+++ Bugology/IssuesViewController.swift
@@ -25,7 +25,7 @@ public class IssuesViewController: UITableViewController {
   public override func viewDidLoad() {
     super.viewDidLoad()
 
-    tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: projectCellIdentifier)
+    tableView.registerNib(UINib(nibName: "IssuesCell", bundle: nil), forCellReuseIdentifier: projectCellIdentifier)
     dataSource.tableViewCellConfiguration = self.configureCell
     dataSource.bindToTableView(tableView)
 
@@ -38,13 +38,14 @@ public class IssuesViewController: UITableViewController {
     title = project.name
 
     client?.getIssuesForProject(project).onSuccess { issues in
-      self.dataSource.data = issues
+      self.dataSource.data = issues.filter { $0.status != "Closed" }
       self.tableView.reloadData()
     }
   }
 
   func configureCell(cell: UITableViewCell, issue: Issue) {
     cell.textLabel?.text = issue.name
+    cell.detailTextLabel?.text = issue.status
   }
 
 }
